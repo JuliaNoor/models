c6b3e28b1c139305573d41e4b9859870963802f7
Fatema Julia <julia@Fatemas-MacBook-Pro.local>
2018-10-13 17:43:12 -0700

    file name change

--- research/audioset/youtube_downloader.py
+++ research/audioset/youtube_downloader.py
@@ -19,12 +19,12 @@ from subprocess import call
 FLAGS = flags.FLAGS
 
 if __name__ == '__main__':
-  flags.DEFINE_string('input_youtube_id_tsv', '/mnt/disks/disk-1/data/youtube_video/youtube_unbalanced_train.txt',
+  flags.DEFINE_string('input_youtube_id_tsv', '/Users/julia/PSVA/data/output/balanced/youtube_balanced_train.txt',
                     'TSV file with lines "<id>\t<start_time>\t<end_time>\t<label>" where '
                     ' and <labels> '
 	            'must be an integer list joined with semi-colon ";"')
 
-  flags.DEFINE_string('output_dir','/mnt/disks/disk-1/data/youtube_video', 'where to save the wav file')
+  flags.DEFINE_string('output_dir','/Users/julia/PSVA/data/output/balanced', 'where to save the wav file')
 
 def main(unused_argv):
   print(FLAGS.input_youtube_id_tsv)
@@ -39,7 +39,7 @@ def main(unused_argv):
       print( youtube_id,st_time, end_time, label)
       #wav_file = FLAGS.output_dir+'/'+str(i)+'.wav'
       wav_file = FLAGS.output_dir+'/'+str(i)
-      sh_cmd = 'youtube-dl -v -o \''+wav_file+'.%(ext)s\'' + ' -x --audio-format wav \'https://www.youtube.com/watch?v=' + youtube_id+'\''
+      sh_cmd = 'youtube-dl --no-check-certificate -v -o \''+wav_file+'.%(ext)s\'' + ' -x --audio-format wav \'https://www.youtube.com/watch?v=' + youtube_id+'\''
       print(sh_cmd)
       #call(sh_cmd);
       os.system(sh_cmd)
@@ -55,6 +55,8 @@ def main(unused_argv):
         newAudio.export(new_wav_file, format="wav") 
         print('--2')
         f.write(new_wav_file+"\t"+st_time+"\t"+end_time+"\t"+label+"\r\n")    
+        rm_cmd = 'rm -rf '+wav_file
+        os.system(rm_cmd)
       #examples_batch = vggish_input.wavfile_to_examples(wav_file)
       #print(examples_batch)
     except:
